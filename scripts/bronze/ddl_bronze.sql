cdd1eeb4d18b6962baf4ef7977507df3377e3afe
stella-vu <Nhunghoangthao@gmail.com>
2025-12-21 16:05:10 +1100

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -5,29 +5,29 @@ use bronze;
 DROP TABLE IF EXISTS bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
 	cst_id INT,
-	cst_key NVARCHAR(50),
-	cst_firstname NVARCHAR(50),
-	cst_lastname NVARCHAR(50),
-	cst_material_status NVARCHAR(50),
-	cst_gndr NVARCHAR(50),
+	cst_key VARCHAR(50),
+	cst_firstname VARCHAR(50),
+	cst_lastname VARCHAR(50),
+	cst_material_status VARCHAR(50),
+	cst_gndr VARCHAR(50),
 	cst_create_date DATE
 );
 
 DROP TABLE IF EXISTS bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
-	prd_key NVARCHAR(50),
-	prd_nm NVARCHAR(100),
+	prd_key VARCHAR(50),
+	prd_nm VARCHAR(100),
 	prd_cost INT,
-	prd_line NVARCHAR(10),
+	prd_line VARCHAR(10),
 	prd_start_dt DATE,
 	prd_end_dt DATE
 );
 
 DROP TABLE IF EXISTS bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
-	sls_ord_num NVARCHAR(50),
-	sls_prd_key NVARCHAR(50),
+	sls_ord_num VARCHAR(50),
+	sls_prd_key VARCHAR(50),
     sls_cust_id INT,
     sls_order_dt INT,
     sls_ship_dt INT,
@@ -39,22 +39,22 @@ CREATE TABLE bronze.crm_sales_details (
 
 DROP TABLE IF EXISTS bronze.epr_cust_az12;
 CREATE TABLE bronze.epr_cust_az12 (
-	cid NVARCHAR(50),
+	cid VARCHAR(50),
     bdate DATE,
-    gen NVARCHAR(50)
+    gen VARCHAR(50)
 );
 
 DROP TABLE IF EXISTS bronze.epr_loc_a101;
 CREATE TABLE bronze.epr_loc_a101 (
-	cid NVARCHAR(50),
-    cntry NVARCHAR(50)
+	cid VARCHAR(50),
+    cntry VARCHAR(50)
 );
 
 DROP TABLE IF EXISTS bronze.epr_px_cat_g1v2;
 CREATE TABLE bronze.epr_px_cat_g1v2 (
-	id NVARCHAR(50),
-	cat NVARCHAR(50),
-	subcat NVARCHAR(50),
-	maintenance NVARCHAR(50)
+	id VARCHAR(50),
+	cat VARCHAR(50),
+	subcat VARCHAR(50),
+	maintenance VARCHAR(50)
 );
     
