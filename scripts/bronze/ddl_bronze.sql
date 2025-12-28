988297fe93a222eb9142f9de036c8a4124461786
stella-vu <Nhunghoangthao@gmail.com>
2025-12-29 09:15:38 +1100

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,6 +1,13 @@
--- Using mySQL
-
-use bronze;
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
 DROP TABLE IF EXISTS bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
@@ -17,7 +24,7 @@ DROP TABLE IF EXISTS bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
 	prd_id INT,
 	prd_key VARCHAR(50),
-	prd_nm VARCHAR(100),
+	prd_nm VARCHAR(50),
 	prd_cost INT,
 	prd_line VARCHAR(10),
 	prd_start_dt DATE,
