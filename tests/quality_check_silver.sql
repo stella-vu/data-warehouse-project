e10c207d7af5c65cf8764447f58f1f236241254c
stella-vu <Nhunghoangthao@gmail.com>
2025-12-30 10:39:06 +1100

    Update quality_check_silver.sql

--- tests/quality_check_silver.sql
+++ tests/quality_check_silver.sql
@@ -10,6 +10,7 @@ Script Purpose:
     - Data standardization and consistency.
     - Invalid date ranges and orders.
     - Data consistency between related fields.
+    - Invisible character like '\r', '\n'
 
 Usage Notes:
     - Run these checks after data loading Silver Layer.
@@ -25,7 +26,7 @@ Usage Notes:
 SELECT 
     cst_id, 
     COUNT(*) 
-FROM bronze.crm_cust_info
+FROM silver.crm_cust_info
 GROUP BY cst_id
 HAVING COUNT(*) > 1 OR cst_id IS NULL;
 
@@ -46,3 +47,124 @@ FROM silver.crm_cust_info;
 -- ====================================================================
 -- Check for NULLs or Duplicates in Primary Key
 -- Expectation: No Results
+SELECT 
+    prd_id,
+    COUNT(*) 
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+SELECT 
+    prd_nm 
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- Check for NULLs or Negative Values in Cost
+-- Expectation: No Results
+SELECT 
+    prd_cost 
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    prd_line 
+FROM silver.crm_prd_info;
+
+-- Check for Invalid Date Orders (Start Date > End Date)
+-- Expectation: No Results
+SELECT 
+    * 
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
+
+-- ====================================================================
+-- Checking 'silver.crm_sales_details'
+-- ====================================================================
+-- Check for Invalid Order Dates
+-- Expectation: No Invalid Dates
+SELECT 
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    NULLIF(sls_order_dt, 0) AS sls_order_dt_1
+FROM silver.crm_sales_details
+WHERE 
+	sls_order_dt <= 0
+    OR CHAR_LENGTH(CAST(sls_order_dt AS CHAR)) != 10
+    OR sls_order_dt > CURRENT_TIMESTAMP()
+    OR sls_order_dt < 2000-01-01;
+
+-- Check Data Consistency: Sales = Quantity * Price
+-- Expectation: No Results
+SELECT DISTINCT 
+    sls_sales,
+    sls_quantity,
+    sls_price 
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+   OR sls_sales IS NULL 
+   OR sls_quantity IS NULL 
+   OR sls_price IS NULL
+   OR sls_sales <= 0 
+   OR sls_quantity <= 0 
+   OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price;
+
+-- ====================================================================
+-- Checking 'silver.erp_cust_az12'
+-- ====================================================================
+-- Identify Out-of-Range Dates
+-- Expectation: Customer's age in range of 18 to 90 years old
+-- Check for customer under 18 or older than 90
+SELECT *,
+TIMESTAMPDIFF(YEAR, bdate, CURDATE())
+- (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(bdate, '%m%d'))
+AS customer_age
+FROM silver.erp_cust_az12
+WHERE 
+	TIMESTAMPDIFF(YEAR, bdate, CURDATE())
+- (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(bdate, '%m%d')) < 18 
+OR TIMESTAMPDIFF(YEAR, bdate, CURDATE())
+- (DATE_FORMAT(CURDATE(), '%m%d') < DATE_FORMAT(bdate, '%m%d')) > 90;
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    gen 
+FROM silver.erp_cust_az12;
+
+-- ====================================================================
+-- Checking 'silver.erp_loc_a101'
+-- ====================================================================
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    cntry 
+FROM silver.erp_loc_a101
+ORDER BY cntry;
+
+-- ====================================================================
+-- Checking 'silver.erp_px_cat_g1v2'
+-- ====================================================================
+-- Check for Invisible charater
+-- Expectation: No Results
+SELECT 
+    id,
+    LENGTH(TRIM(id))
+FROM silver.erp_px_cat_g1v2
+WHERE id like '\n%' or id like '\r%';
+
+-- Check for Unwanted Spaces
+-- Expectation: No Results
+SELECT 
+    * 
+FROM silver.erp_px_cat_g1v2
+WHERE cat != TRIM(cat) 
+   OR subcat != TRIM(subcat) 
+   OR maintenance != TRIM(maintenance);
+
+-- Data Standardization & Consistency
+SELECT DISTINCT 
+    maintenance 
+FROM silver.erp_px_cat_g1v2;
