bcec70c719c7a4bc156d3656a75ef0c042b0a77e
stella-vu <Nhunghoangthao@gmail.com>
2025-12-30 09:54:00 +1100

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -7,51 +7,58 @@ CREATE TABLE silver.crm_cust_info (
 	cst_lastname VARCHAR(50),
 	cst_material_status VARCHAR(50),
 	cst_gndr VARCHAR(50),
-	cst_create_date DATE
+	cst_create_date DATE,
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
 	prd_id INT,
+	cat_id VARCHAR(50),
 	prd_key VARCHAR(50),
-	prd_nm VARCHAR(100),
+	prd_nm VARCHAR(50),
 	prd_cost INT,
-	prd_line VARCHAR(10),
+	prd_line VARCHAR(50),
 	prd_start_dt DATE,
-	prd_end_dt DATE
+	prd_end_dt DATE,
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.crm_sales_details;
 CREATE TABLE silver.crm_sales_details (
 	sls_ord_num VARCHAR(50),
 	sls_prd_key VARCHAR(50),
-    sls_cust_id INT,
-    sls_order_dt INT,
-    sls_ship_dt INT,
-    sls_due_dt INT,
+    sls_cust_id VARCHAR(50),
+    sls_order_dt DATE,
+    sls_ship_dt DATE,
+    sls_due_dt DATE,
     sls_sales INT,
     sls_quantity INT,
-    sls_price INT
+    sls_price INT,
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-DROP TABLE IF EXISTS silver.epr_cust_az12;
-CREATE TABLE silver.epr_cust_az12 (
+DROP TABLE IF EXISTS silver.erp_cust_az12;
+CREATE TABLE silver.erp_cust_az12 (
 	cid VARCHAR(50),
     bdate DATE,
-    gen VARCHAR(50)
+    gen VARCHAR(50),
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-DROP TABLE IF EXISTS silver.epr_loc_a101;
-CREATE TABLE silver.epr_loc_a101 (
+DROP TABLE IF EXISTS silver.erp_loc_a101;
+CREATE TABLE silver.erp_loc_a101 (
 	cid VARCHAR(50),
-    cntry VARCHAR(50)
+    cntry VARCHAR(50),
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-DROP TABLE IF EXISTS silver.epr_px_cat_g1v2;
-CREATE TABLE silver.epr_px_cat_g1v2 (
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
+CREATE TABLE silver.erp_px_cat_g1v2 (
 	id VARCHAR(50),
 	cat VARCHAR(50),
 	subcat VARCHAR(50),
-	maintenance VARCHAR(50)
+	maintenance VARCHAR(50),
+	dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
     
