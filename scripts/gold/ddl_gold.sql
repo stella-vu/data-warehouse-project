8aec9d1c0b306482ef9382622569c46301e02496
stella-vu <Nhunghoangthao@gmail.com>
2025-12-30 10:06:14 +1100

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -13,3 +13,78 @@ Usage:
     - These views can be queried directly for analytics and reporting.
 ===============================================================================
 */
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
+DROP VIEW IF EXISTS gold.dim_customer; 
+
+CREATE VIEW gold.dim_customer AS
+SELECT
+	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS first_name,
+    ci.cst_lastname AS last_name,
+    ci.cst_material_status AS material_status,
+    CASE 
+		WHEN ci.cst_gndr = "n/a" THEN COALESCE(ca.gen, 'n/a') -- Fallback to ERP data
+        ELSE ci.cst_gndr -- CRM is the primary source for gender
+	END gender,
+    ca.bdate AS birthday,
+	ci.cst_create_date AS create_date
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.erp_cust_az12 AS ca
+	ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la
+	ON ci.cst_key = la.cid;
+    
+-- =============================================================================
+-- Create Dimension: gold.dim_products
+-- =============================================================================
+DROP VIEW IF EXISTS gold.dim_products;
+
+CREATE VIEW gold.dim_products AS
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- Surrogate key
+	pn.prd_id AS product_id,
+    pn.prd_key AS product_number,
+    pn.prd_nm AS product_name,
+	pn.cat_id AS category_id,
+    pc.cat AS category,
+    pc.subcat AS subcategory,
+    pc.maintenance,
+    pn.prd_cost AS cost,
+    pn.prd_line AS product_line,
+    pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+	ON pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
+
+SELECT * FROM gold.dim_products;
+
+-- =============================================================================
+-- Create Fact: gold.fact_sales
+-- =============================================================================
+DROP VIEW IF EXISTS gold.fact_sales;
+CREATE VIEW gold.fact_sales AS
+SELECT 
+	sd.sls_ord_num,
+    pr.product_number,
+	cu.customer_id,
+	sd.sls_order_dt,
+	sd.sls_ship_dt,
+	sd.sls_due_dt,
+	sd.sls_sales,
+	sd.sls_quantity,
+	sd.sls_price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+	ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customer cu
+	ON sd.sls_cust_id = cu.customer_id;
+
+-- Check data from gold_layer
+SELECT * FROM gold.dim_customer;
+SELECT * FROM gold.dim_products;
+SELECT * FROM silver.crm_sales_details;
