346c2871162ca0dd4abde5c5bc09f65ba3b18167
stella-vu <Nhunghoangthao@gmail.com>
2025-12-30 09:54:46 +1100

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,4 +1,14 @@
-use silver;
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
 DROP TABLE IF EXISTS silver.crm_cust_info;
 CREATE TABLE silver.crm_cust_info (
 	cst_id INT,
