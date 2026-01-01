93ecbefa24343339a93eaefc4fd2029138338533
stella-vu <Nhunghoangthao@gmail.com>
2026-01-02 09:37:45 +1100

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -69,15 +69,15 @@ SELECT * FROM gold.dim_products;
 DROP VIEW IF EXISTS gold.fact_sales;
 CREATE VIEW gold.fact_sales AS
 SELECT 
-	sd.sls_ord_num,
-    pr.product_number,
-	cu.customer_id,
-	sd.sls_order_dt,
-	sd.sls_ship_dt,
-	sd.sls_due_dt,
-	sd.sls_sales,
-	sd.sls_quantity,
-	sd.sls_price
+	sd.sls_ord_num AS order_number,
+    pr.product_number AS product_number,
+	cu.customer_id AS customer_id,
+	sd.sls_order_dt AS order_date,
+	sd.sls_ship_dt AS shipping_date,
+	sd.sls_due_dt AS due_date,
+	sd.sls_sales AS sales,
+	sd.sls_quantity AS quantity,
+	sd.sls_price AS price
 FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
 	ON sd.sls_prd_key = pr.product_number
